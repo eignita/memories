87fa3e799a6ffdb64ca293aba7c0385bbf44ed89
notch raiser <notch@raiservm.mshome.net>
2020-11-23 14:01:55 +0530

    updated memories component

--- client/src/components/Memories/Memories.js
+++ client/src/components/Memories/Memories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Memory from './Memory/Memory';
 import useStyles from './styles';
@@ -8,11 +9,17 @@ function Memories() {
     const memories = useSelector(state => state.memories);
     console.log(memories);
     return (
-        <>
-            <h1>MEMORIES</h1>
-            <Memory />
-            <Memory />
-        </>
+        !memories.length ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {
+                    memories.map(memory => (
+                        <Grid item key={memory._id} xs={12} sm={6}>
+                            <Memory memory={memory} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     )
 }
 
